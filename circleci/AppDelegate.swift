8b43b561425e9913bb7aec7b1848f6a877219320
buddybuild <help@buddybuild.com>
2017-04-10 06:43:28 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- circleci/AppDelegate.swift
+++ circleci/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         // Override point for customization after application launch.
         
         Fabric.with([Crashlytics.self])
